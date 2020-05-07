9c3f8a2fbd6d93a3721f74b38330b253c1d6b7fb
LingrenKong <1551086871@qq.com>
2020-05-07 20:30:35 +0800

    修改了原本二分类模型检验的小问题，增加了多分类模型检验

--- Algorithm/其他/小工具.py
+++ Algorithm/其他/小工具.py
@@ -24,12 +24,36 @@ def npCheck(arr,display=False):
     print('-----------')
 
 def modelCheck(y,pred):
+    """
+    二分类数据的模型检验综合
+    """
     import numpy as np
     from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
-    print("错误率error：",np.mean(y==pred))
+    print("错误率error：",1-np.mean(y==pred))
     print("查准率precision：",precision_score(y,pred))
     print("查全率recall：",recall_score(y,pred))
     print("F1 score：",f1_score(y,pred))
-    print(confusion_matrix(y,pred))
+    print("混淆矩阵如下：\n",confusion_matrix(y,pred))
     return
 
+def modelCheck_Mul(y,pred):
+    """
+    多变量数据的模型检验
+    """
+    import numpy as np
+    from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+    print("错误率error：",1-np.mean(y==pred))
+    print("查准率precision：\n分项：",
+          precision_score(y,pred,average=None),
+          "\n宏：",precision_score(y,pred,average='macro'),
+          "\n微：",precision_score(y,pred,average='micro'))
+    print("查全率recall：\n分项：",
+          recall_score(y,pred,average=None),
+          "\n宏：",recall_score(y,pred,average='macro'),
+          "\n微：",recall_score(y,pred,average='micro'))
+    print("F1 score：\n分项：",
+          f1_score(y,pred,average=None),
+          "\n宏：",f1_score(y,pred,average='macro'),
+          "\n微：",f1_score(y,pred,average='micro'))
+    print("混淆矩阵如下：\n",confusion_matrix(y,pred))
+    return
